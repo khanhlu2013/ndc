e5fa649c2a3a52030bbada843c841687ee09cb37
Khanh Tran <khanhlu2013@gmail.com>
2016-03-02 01:12:03 -0800

    add angular2

---
+++ static/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from 'angular2/core';
+
+@Component({
+	selector : 'my-app',
+	template :`
+		<h1>{{title}}</h1>
+	`
+})
+export class AppComponent{
+	title = 'Hello world';
+}
