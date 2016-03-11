6a71d835ef415766be3c9fd936833e68a1e9cb37
Khanh Tran <khanhlu2013@gmail.com>
2016-03-10 18:46:10 -0800

    fix sample_test to pass to test travis

--- test/sample_test.js
+++ test/sample_test.js
@@ -1,6 +1,6 @@
 describe("a sample test for travis", function () {
 
     it("can be executed and failed",function(){
-        expect(1).toEqual(2);      
+        expect(2).toEqual(2);      
     })
 });
