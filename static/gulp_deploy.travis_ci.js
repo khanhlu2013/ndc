e9b5b4607ac930b01dfe57e8da58bb6e23ed38fa
Khanh Tran <khanhlu2013@gmail.com>
2016-03-23 14:56:36 -0700

    add initial build for travis

---
+++ static/gulp_deploy.travis_ci.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var shell = require('gulp-shell')
+var ts = require('gulp-typescript');
+var uglify = require('gulp-uglify');
+
+gulp.task('install_typings', shell.task([
+  'typings install',
+]))
+
+var tsProject = ts.createProject('tsconfig.json',{outFile:'ndc_bundle.js'});
+gulp.task('default',['install_typings'],function(){
+	var tsResult = 
+		 tsProject.src()
+		.pipe(ts(tsProject));
+
+	return tsResult.js
+		.pipe(uglify())	
+		.pipe(gulp.dest('build/deploy'));	
+});
