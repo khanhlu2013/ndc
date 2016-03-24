e9025e8e61bd4871d6c0e85ff1798348b560385e
Khanh Tran <khanhlu2013@gmail.com>
2016-03-23 19:51:46 -0700

    exclude devDependency from ./static/package.json for deployment

--- static/gulp_deploy.js
+++ static/gulp_deploy.js
@@ -1,15 +1,11 @@
 //this file is run on travis ci server
 var gulp = require('gulp');
-var shell = require('gulp-shell')
 var ts = require('gulp-typescript');
 var uglify = require('gulp-uglify');
 
-gulp.task('install_typings', shell.task([
-  'typings install',
-]))
 
 var tsProject = ts.createProject('tsconfig.json',{outFile:'ndc_bundle.js'});
-gulp.task('default',['install_typings'],function(){
+gulp.task('default',function(){
 	var tsResult = 
 		 tsProject.src()
 		.pipe(ts(tsProject));
