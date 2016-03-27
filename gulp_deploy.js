efd3938d52dd91795b282ff0776b372da062c8d8
Khanh Tran <khanhlu2013@gmail.com>
2016-03-26 22:19:04 -0700

    add deploy to firebase in gulp_deploy.js

--- gulp_deploy.js
+++ gulp_deploy.js
@@ -4,6 +4,7 @@ var ts = require('gulp-typescript');
 var uglify = require('gulp-uglify');
 var shell = require('gulp-shell');
 var run_sequence = require('run-sequence');
+var client = require('firebase-tools');
 
 var tsProject = ts.createProject('./static/tsconfig.json',{outFile:'ndc_bundle.js'});
 gulp.task('compile_ts',function(){
@@ -20,10 +21,25 @@ gulp.task('collect_static', shell.task([
   'honcho run python manage.py collectstatic --noinput -i .bin -i angular2 -i es6-promise -i es6-shim -i reflect-metadata -i rxjs -i systemjs -i typings -i zone.js'
 ]));
 
+gulp.task('deploy_2_firebase',function(cb){
+	client.deploy.rules({
+		firebase: 'ndc-static',
+		token: 'qjagr952EcPJKsDxeaZiNqVx4ZZXALCuTzeeRslp',
+		cwd: './static_root'
+	}).then(function() {
+		cb();
+	}).catch(function(err) {
+		cb(err);
+	});
+});
+
 gulp.task('default',function(cb){
 	run_sequence(
 		'compile_ts',
 		'collect_static',
+		'deploy_2_firebase',
 		cb
 	);
 });
+
+
