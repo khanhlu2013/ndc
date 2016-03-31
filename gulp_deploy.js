a5d5c71708e47640eb883a905196c95a85859732
Khanh Tran <khanhlu2013@gmail.com>
2016-03-30 17:43:25 -0700

    add migration to deploy;add pre-cleanup on gulp-collect-js-lib;fix template html comment on template tag

--- gulp_deploy.js
+++ gulp_deploy.js
@@ -17,10 +17,11 @@ gulp.task('compile_ts',function(){
 });
 
 gulp.task('collect_static', shell.task([	
-  'honcho run python manage.py collectstatic --noinput'
+  'python manage.py collectstatic --noinput'
+  //make sure we set DISABLE_COLLECTSTATIC = true @ heroku. if we don't, it is not a fatal problem. heroku will smart enough to detect unmodification.
 ]));
 
-gulp.task('post_deploy_clean_up',shell.task([
+gulp.task('clean_up',shell.task([
 	'rm -rf ./node_modules',
 	'rm -rf ./static/node_modules',
 	'rm -rf ./static/typings'
@@ -30,6 +31,7 @@ gulp.task('default',function(cb){
 	run_sequence(
 		'compile_ts',
 		'collect_static',
+		'clean_up',
 		cb
 	);
 });
--- static/gulp_collect_js_lib.js
+++ static/gulp_collect_js_lib.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var shell = require('gulp-shell');
 
 gulp.task('default',shell.task([
+	'rm -rf collected_js_lib', 
 	'mkdir collected_js_lib',
 	'cp ./node_modules/es6-shim/es6-shim.min.js ./collected_js_lib',
 	'cp ./node_modules/systemjs/dist/system-polyfills.js ./collected_js_lib',
